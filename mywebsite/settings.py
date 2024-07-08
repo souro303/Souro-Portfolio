f099f637c0127d20a3ded03fc9999c6662ac1483
souro303 <sourovedatta5@gmail.com>
2024-07-09 01:15:55 +0600

    Initial commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -103,11 +103,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
+
+
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
