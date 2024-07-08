8e470772350278ac33ce7fc45e4a5ad9ebb35ce1
souro303 <sourovedatta5@gmail.com>
2024-07-09 01:36:38 +0600

    Initial commit

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -102,10 +102,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Adjusted to 'staticfiles'
+
+# Additional directories where static files are located
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static'),  # Directory where your CSS, JS, and images reside
 ]
+
 
 
 MEDIA_URL = '/images/'
